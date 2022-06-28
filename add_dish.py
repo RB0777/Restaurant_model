ed29dc87a8e153ba2e2880c6a9219f99adc8aabc
RB0777 <98003943+RB0777@users.noreply.github.com>
2022-06-29 02:29:32 +0530

    Add files via upload

--- add_dish.py
+++ add_dish.py
@@ -1,17 +1,24 @@
 #Importing required libraries
 
 import pandas as pd
+import pymongo
+connectionstring="mongodb+srv://rb0777:Galaxys9plus@cluster0.zuny1ac.mongodb.net/test"
+myclient = pymongo.MongoClient(connectionstring)
+#Creating database
+mydb = myclient["Restaurant_model"]
+#Creating collection
+posts_1=mydb.add_dishes
 
 # Function for adding dishes
 def my_Dish(Name,Price,Description,Non_Veg,Veg,SpiceLevel):
-    try:    
-        # Reading  excel file    
-            df = pd.read_excel("Menu.xlsx")
-            # Adding required columns
-            df = df.append({"Name":Name,"Price":Price,"Description":Description,"Non_Veg":Non_Veg,"Veg":Veg,"SpiceLevel":SpiceLevel}, ignore_index=True)
-            # Saving excel file
-            df.to_excel("Menu.xlsx",index=False)    
-            return "Dish_added"
-    except:
-        return "Dish can't be added"        
+        
+    try:
+        # Adding required columns
+        post_id2 = posts_1.insert_one({"Name":Name,"Price":Price,"Description":Description,"Non_Veg":Non_Veg,"Veg":Veg,"SpiceLevel":SpiceLevel}).inserted_id
 
+        print(post_id2)
+        return "Dish_added"
+    except:
+        return "Dish not added"        
+    
+                
