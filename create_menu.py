7e634ae79ea507aa0853843d8a98bd3da5a945b8
RB0777 <98003943+RB0777@users.noreply.github.com>
2022-06-29 02:34:35 +0530

    Add files via upload

--- create_menu.py
+++ create_menu.py
@@ -1,15 +1,18 @@
 #Importing required libraries
 import pandas as pd
-
+import pymongo
+connectionstring="mongodb+srv://rb0777:Galaxys9plus@cluster0.zuny1ac.mongodb.net/test"
+myclient = pymongo.MongoClient(connectionstring)
+#Creating database
+mydb = myclient["Restaurant_model"]
+posts_3=mydb.create_menu
 # Function for creating menu
 def Create_Menu(Name,Choice,Quarter,Half,Full):
-        try:
-            # Reading  excel file    
-            df = pd.read_excel("Create_Menu.xlsx")
+        
+    try:
             # Adding required columns
-            df = df.append({"DISH_NAME":Name,"VEG/NON-VEG":Choice,"PRICE:QUARTER":Quarter,"PRICE:HALF":Half,"PRICE:FULL":Full}, ignore_index=True)
-            # Saving excel file
-            df.to_excel("Create_Menu.xlsx",index=False)
+            post_id3 = posts_3.insert_one({"DISH_NAME":Name,"VEG/NON-VEG":Choice,"PRICE:QUARTER":Quarter,"PRICE:HALF":Half,"PRICE:FULL":Full}).inserted_id
+            print(post_id3)
             return "Menu_Created"
-        except:
-            return "Menu can't be created"
+    except:
+        return"Menu not created"    
