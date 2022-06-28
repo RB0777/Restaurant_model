2959599b27ce973282a2b8f707b8c5efed1f4a68
RB0777 <98003943+RB0777@users.noreply.github.com>
2022-06-29 02:42:27 +0530

    Add files via upload

--- bill.py
+++ bill.py
@@ -1,8 +1,23 @@
 import pandas as pd
 import get_location as gl
+import generate_bill as gb
 def bill_generate(UID):
-    
-    order = pd.read_excel("orders.xlsx")
-    oi=gl.getIndexes(order,UID)
-    print(order)
-bill_generate(1.38148922895775E+38)
+    try:
+        order = pd.read_excel("orders.xlsx")
+        oi=gl.getIndexes(order,UID)
+        print(oi)
+        menu = pd.read_excel("Menu.xlsx")
+        d_name=(order['DISH_NAME'].iloc[oi[0]])
+        d_qty=(order['QUANTITY'].iloc[oi[0]])
+        
+        di=gl.getIndexes(menu,d_name)
+        print(di)
+        qty=(d_qty)
+        # mn=gl.getIndexes(menu,Price)
+        price=(menu['Price'].iloc[di[0]])
+        total=(qty*price)
+        bl=gb.report(UID,d_name,qty,total)
+
+        return"Bill generated successfully"
+    except:
+        return"Bill generation failed"
